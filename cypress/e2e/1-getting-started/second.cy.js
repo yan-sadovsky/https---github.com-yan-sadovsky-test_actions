7cb4121989fbbb52d27d5c6bd22a9374384a9045
Yan <yan.sadovsky@issuerdirect.com>
2024-04-05 12:46:56 +0200

    fail test

--- cypress/e2e/1-getting-started/second.cy.js
+++ cypress/e2e/1-getting-started/second.cy.js
@@ -3,6 +3,7 @@ describe('First test', () => {
   urls.forEach((el, index)=>{
     it(`Test test ${index + 1}`, () => {
       cy.visit(el)
+      cy.url().should('contain', "www.youtube.com")
     })
   })
  
