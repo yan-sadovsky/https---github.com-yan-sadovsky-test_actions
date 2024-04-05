7e75ed5b033de9ac8013c839aa32160ee91f97c2
Yan <yan.sadovsky@issuerdirect.com>
2024-04-05 12:32:00 +0200

    parallel test

---
+++ cypress/e2e/1-getting-started/second.cy.js
@@ -0,0 +1,9 @@
+let urls = ["https://www.youtube.com/", "https://www.github.com/"]
+describe('First test', () => {
+  urls.forEach((el, index)=>{
+    it(`Test test ${index + 1}`, () => {
+      cy.visit(el)
+    })
+  })
+ 
+})
