5947fa0f5ac56cc230d4f39870134b2f5941e7e1
Yan <yan.sadovsky@issuerdirect.com>
2024-04-05 16:27:58 +0200

    test run

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,10 +1,14 @@
 
 import { defineConfig } from "cypress";
+import {allureCypress} from "allure-cypress/reporter"
+import * as dotenv from "dotenv"
+dotenv.config()
 
 export default defineConfig({
   projectId: 'nj2ibf',
   e2e: {
     setupNodeEvents(on, config) {
+      allureCypress(on);
       // implement node event listeners here
     },
   },
